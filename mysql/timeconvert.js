252a8c77102b9992d9d7ce6c279931eff60fefd0
Neal Mastel <nealmastel@gmail.com>
2019-08-01 21:14:43 -0500

    Figured out UNIX_TIMESTAMP(timestamp) to array

--- mysql/timeconvert.js
+++ mysql/timeconvert.js
@@ -1,5 +1,10 @@
-obj =  {
-    thermocouple: 1,
-    temperature: 188,
-    'UNIX_TIMESTAMP(timestamp)': 1561328618 }
-console.log(Object.values(obj[1]));
+var json = {
+    "a(test)" : "one", "b":2, "c":3
+} 
+console.log(json)
+
+json["A"] = json["a(test)"];
+
+delete json["a(test)"];
+
+console.log(json.A)
